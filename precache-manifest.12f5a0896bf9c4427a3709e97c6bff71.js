6a8816053afc6293b06ffa49fd0be614c30df1c0
unknown <oske.design@gmail.com>
2020-04-13 03:33:21 +0500

    Updates

---
+++ precache-manifest.12f5a0896bf9c4427a3709e97c6bff71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d35b882bcef013acf4e2ce1619d2a0",
+    "url": "/foodlog/index.html"
+  },
+  {
+    "revision": "bc9352f75f8fabdc407d",
+    "url": "/foodlog/static/css/main.41daa917.chunk.css"
+  },
+  {
+    "revision": "4dd00be2397f04a339da",
+    "url": "/foodlog/static/js/2.0888eacd.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/foodlog/static/js/2.0888eacd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9352f75f8fabdc407d",
+    "url": "/foodlog/static/js/main.b5419f60.chunk.js"
+  },
+  {
+    "revision": "2a82fe8d4ef04bac98ce",
+    "url": "/foodlog/static/js/runtime-main.dfea36eb.js"
+  }
+]);
